0b4b411c4a89cf385de20e0960893e0284c6a84f
Consuelo Vereau <168292313+ConsueloVT@users.noreply.github.com>
2025-05-30 21:35:49 -0300

    Ya se guardan los cambios de EditarAlojamiento

--- AL.Aplicacion/obj/Debug/net8.0/AL.Aplicacion.AssemblyInfo.cs
+++ AL.Aplicacion/obj/Debug/net8.0/AL.Aplicacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AL.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f730d458372038f1357caf4fed0f0ecaabed7335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32a48101a8f686aaec3c4999da25949ae3c08c20")]
 [assembly: System.Reflection.AssemblyProductAttribute("AL.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AL.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
