b3b9918efc474602b4b8b8a53101b81d4032b6b1
M. Agustina Olia <agustinaolia2@gmail.com>
2025-06-19 21:46:32 -0300

    Mejoré el frontend de Solicitar Reserva

--- AL.Aplicacion/Entidades/Reserva.cs
+++ AL.Aplicacion/Entidades/Reserva.cs
@@ -12,8 +12,15 @@ public class Reserva
     public double MontoEstadia { get; set; }
     public String? EstadoCheckOut { get; set; }
     public String? EstadoReserva { get; set; }
-    public int CantidadDeAdultos { get; set; }
-    public int CantidadDeNiños { get; set; }
+    [Required(ErrorMessage = "Ingrese al menos un adulto.")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad de adultos debe ser al menos 1.")]
+    public int? CantidadDeAdultos { get; set; }
+    
+    [Required(ErrorMessage = "Por favor ingresá la cantidad de niños.")]
+    [Range(0, int.MaxValue, ErrorMessage = "La cantidad de niños debe ser igual o mayor a 0")]
+    public int? CantidadDeNiños { get; set; }
+
+
     public String InformacionInquilinos { get; set; } = string.Empty;
     public List<String> ListaInformacionAdicional { get; set; } = new List<String>();
 
